18e3001e752cd49315831acb4c793693faa550ac
Boar <hebingchang1@live.com>
2017-07-08 22:38:24 +0800

    update code

--- src/index.js
+++ src/index.js
@@ -20,9 +20,35 @@ var errorMsg = "";
 var ruff_status = "";
 var status_code = 0;
 
+function Air_conditioner(){
+	var air = new Object;
+	air.power = 0;
+	air.temperature = 16;
+	air.mode = 0;
+	air.getStatus = function() {
+		return {"power": air.power, "temperature": air.temperature, "mode": air.mode};
+	};
+	air.setStatus = function(power, temp, mode) {
+		air.power = power;
+		air.temperature = temp;
+		air.mode = mode;
+	}
+	return air;
+}
+
+var air = Air_conditioner();
+
 function setStatus(st, code) {
 	ruff_status = st;
 	status_code = code;
+}
+
+function showStatus() {
+	var power = ["Power OFF", "Power ON"];
+	var mode = ["Cold", "Heat", "Dehumidify"];
+	var sta = air.getStatus();
+	console.log(sta);
+	showText(power[sta["power"]], mode[sta["mode"]] + " " + air.temperature + "C");
 }
 
 function getStatus() {
@@ -42,9 +68,11 @@ function controlRelay(open){
     relayOpen = open;
 }
 
-function showText(txt) {
+function showText(txt1, txt2) {
 	$('#lcd1602').clear();
-	$('#lcd1602').print(txt);
+	$('#lcd1602').print(txt1);
+	$('#lcd1602').setCursor(0, 1);
+	$('#lcd1602').print(txt2);
 }
 
 $.ready(function (error) {
@@ -52,27 +80,40 @@ $.ready(function (error) {
     console.log(error);
         return;
     }
-    netControl.listenCMD({
-        exeCmd:function (cmd) {
-            switch(cmd)
-			{
-				case "c0_00":
-				  showText("Power ON/OFF");
-				  setStatus("success", 1);
-				  break;
-				case "c0_01":
-				  showText("Temperature +");
-				  setStatus("success", 1);
-				  break;
-				case "c0_02":
-				  showText("Temperature -");
-				  setStatus("success", 1);
-				  break;
-				default:
-				  setStatus("Command not found.", 0);
-				  break;
+    
+	// 监听
+	netControl.listenCMD({
+        exeCmd:function (cmd, power, temperature, mode) {
+			if (power === "0" || power === "1") {
+				air.setStatus(power, temperature, mode);
+			} else {
+				console.log(cmd);
+				switch(cmd)
+				{
+					case "c0_00":
+					  if (air.power === "1") {
+						  air.power = "0";
+					  } else {
+						  air.power = "1";
+					  }
+					  break;
+					case "c0_01":
+					  if (parseInt(air.temperature) < 30) {
+						air.temperature = parseInt(air.temperature)+1;
+					  }
+					  break;
+					case "c0_02":
+					  if (parseInt(air.temperature) > 16) {
+					    air.temperature = parseInt(air.temperature)-1;
+					  }
+					  break;
+					default:
+					  setStatus("Command not found.", 0);
+					  break;
+				}
 			}
-
+			
+			showStatus();
         },
         getReply: function () {
            return JSON.stringify({"success": getStatus()[0], "message": getStatus()[1]});
