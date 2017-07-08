9386d31effe121e3a32a58b377904534954e509d
Evangeline <yuanzhehui0325@outlook.com>
2017-07-08 21:34:47 +0800

    Improved indentation

--- src/netControl.js
+++ src/netControl.js
@@ -1,19 +1,23 @@
 'use strict'; 
-        function createServer(callback) {
-             var PORT = 3000;
-             var http = require('http');
-             var url=require('url');
-             var queryString  = require("querystring");
-             var datas={};
-             var server = http.createServer(function (request, response) {
-                 var query=url.parse(request.url).query;
-                 datas = queryString.parse(query);
-                 callback.exeCmd(datas.cmd);
-                 response.write(callback.getReply());
-                 response.end();
-           });
-        server.listen(PORT);
-        console.log("Server runing at port: " + PORT + ".");
-      }
-      module.exports.listenCMD = createServer;
+
+function createServer(callback) {
+	 var PORT = 3000;
+	 var http = require('http');
+	 var url=require('url');
+	 var queryString  = require("querystring");
+	 var datas={};
+	 var server = http.createServer(function (request, response) {
+		 var query=url.parse(request.url).query;
+		 datas = queryString.parse(query);
+		 callback.exeCmd(datas.cmd);
+		 response.write(callback.getReply());
+		 response.end();
+	});
+	
+	server.listen(PORT);
+	console.log("Server runing at port: " + PORT + ".");
+	
+}
+
+module.exports.listenCMD = createServer;
 
